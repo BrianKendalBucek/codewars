0755527e9f3117f26f3bdaf2372ed9e3bab67566
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-08-09 01:00:39 -0700

    tribonacci exercise completed

---
+++ tribonacci.js
@@ -0,0 +1,28 @@
+"use strict";
+function tribonacci([a, b, c], n) {
+    let first = 0;
+    let second = 1;
+    let third = 2;
+    let initialArray = [a, b, c];
+    for (let i = 0; i < initialArray.length; i++) {
+        if (n === 0) {
+            return [];
+        }
+        if (n < 3) {
+            return [n];
+        }
+        if (initialArray.length < n) {
+            //   add the three initial numbers together to get the following number
+            const added = initialArray[first] + initialArray[second] + initialArray[third];
+            //   add this number to the end of the array
+            initialArray.push(added);
+            // calculate next 3 numbers
+            first++;
+            second++;
+            third++;
+        }
+    }
+    return initialArray;
+}
+const test = tribonacci([1, 1, 1], 10);
+console.log(test);
---
+++ tribonacci.ts
@@ -0,0 +1,38 @@
+
+function tribonacci([a, b, c]: [number, number, number], n: number): number[] {
+
+  let first: number = 0;
+  let second: number = 1;
+  let third: number = 2;
+
+  let initialArray = [a, b, c];
+  
+  for (let i = 0; i < initialArray.length; i++) {
+    
+    if (n === 0) {
+      return [];
+    }
+    
+    if (n < 3) {
+      return [n];
+    }
+
+    if (initialArray.length < n) {
+      //   add the three initial numbers together to get the following number
+      const added = initialArray[first] + initialArray[second] + initialArray[third];
+    
+      //   add this number to the end of the array
+      initialArray.push(added);
+  
+      // calculate next 3 numbers
+      first++;
+      second++;
+      third++;
+    }
+  }
+  
+  return initialArray; 
+}
+
+const test = tribonacci([1, 1, 1], 10);
+console.log(test);
