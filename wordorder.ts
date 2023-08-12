903783d33b2b245390e4bc393812e4a424d1e2fd
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-08-11 22:05:29 -0700

    completed word order via embeded number exercise

---
+++ wordorder.ts
@@ -0,0 +1,33 @@
+function order(words: string): string {
+  const regex = /[0-9]/;
+  const orderedNumberStrings = [];
+  const resultArray = [];
+
+  for (const char of words) {
+    if (regex.test(char) === true) {
+      orderedNumberStrings.push(char);
+    }
+  }
+
+  const stringToNumbers = orderedNumberStrings.map(Number);
+
+  const sortedNumbers = stringToNumbers.slice().sort((a, b) => a - b);
+
+  const numbersToString = sortedNumbers.toString();
+
+  const wordArray = words.split(" ").map((str) => str.trim());
+
+  for (const digit of numbersToString) {
+    for (const word of wordArray) {
+      if (word.includes(digit)) {
+        resultArray.push(word);
+      }
+    }
+  }
+
+  const result = resultArray.join(" ");
+
+  return result;
+}
+
+order("4of Fo1r pe6ople g3ood th5e the2");
