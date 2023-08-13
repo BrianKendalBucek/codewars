60d186e83ea5d83da1dc9c2c167369ec5e0f2370
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-08-12 22:23:54 -0700

    solution for population calculation problem

---
+++ populationCalc.js
@@ -0,0 +1,25 @@
+"use strict";
+const nbYear = (p0, percent, aug, p) => {
+    const percentToDecimal = percent / 100;
+    let alteredPopulation = p0;
+    let counter = 0;
+    const recalculate = (currentPopulation, targetPopulation) => {
+        if (currentPopulation >= targetPopulation) {
+            return;
+        }
+        else {
+            const inhabitants = currentPopulation + currentPopulation * percentToDecimal + aug;
+            const inhabitantsRounded = Math.floor(inhabitants);
+            counter += 1;
+            recalculate(inhabitantsRounded, targetPopulation);
+        }
+    };
+    recalculate(alteredPopulation, p);
+    // return counter;
+    console.log(counter);
+};
+nbYear(1500, 5, 100, 5000);
+nbYear(1500000, 2.5, 10000, 2000000);
+nbYear(1500000, 0.25, 1000, 2000000);
+nbYear(1500000, 0.25, -1000, 2000000);
+nbYear(1000, 2.0, 50, 2014);
---
+++ populationCalc.ts
@@ -0,0 +1,37 @@
+const nbYear = (
+  p0: number,
+  percent: number,
+  aug: number,
+  p: number
+): number => {
+  const percentToDecimal = percent / 100;
+  let alteredPopulation = p0;
+  let counter = 0;
+
+  const recalculate = (
+    currentPopulation: number,
+    targetPopulation: number
+  ) => {
+    if (currentPopulation >= targetPopulation) {
+      return;
+    } else {
+      const inhabitants = currentPopulation + currentPopulation * percentToDecimal + aug;
+      const inhabitantsRounded = Math.floor(inhabitants);
+      counter += 1;
+      recalculate(inhabitantsRounded, targetPopulation);
+    }
+  };
+
+  recalculate(alteredPopulation, p);
+  // return counter;
+  console.log(counter)
+};
+
+
+
+nbYear(1500, 5, 100, 5000);
+nbYear(1500000, 2.5, 10000, 2000000);
+nbYear(1500000, 0.25, 1000, 2000000);
+nbYear(1500000, 0.25, -1000, 2000000);
+nbYear(1000, 2.0, 50, 2014);
+
